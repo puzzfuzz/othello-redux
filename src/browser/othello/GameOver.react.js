6e6239e9ee43e2f9c0997c818e9b6c47d401edc7
puzzfuzz <chris.puzzo@gmail.com>
2015-11-16 22:49:01 -0500

    Added some game-over style

--- src/browser/othello/GameOver.react.js
+++ src/browser/othello/GameOver.react.js
@@ -1,6 +1,9 @@
 import React, {PropTypes} from 'react';
 import Component from 'react-pure-render/component';
 
+if (process.env.IS_BROWSER)
+  require('./GameOver.styl');
+
 export default class GameOver extends Component {
 
   static propTypes = {
